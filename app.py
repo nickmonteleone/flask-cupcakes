8565caed9a604254291871749fdf19bf7e816c26
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-12-11 11:56:08 -0800

    added get route for all cupcakes

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 """Flask app for Cupcakes"""
+
+import os
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+
+@app.get('/api/cupcakes')
+def get_cupcakes_data():
+    '''Return data about all cupcakes'''
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
--- seed.py
+++ seed.py
@@ -15,8 +15,8 @@ c2 = Cupcake(
     flavor="chocolate",
     size="small",
     rating=9,
-    image_url="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/"
-          "chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+    image_url="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/" +
+    "chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
 )
 
 db.session.add_all([c1, c2])
